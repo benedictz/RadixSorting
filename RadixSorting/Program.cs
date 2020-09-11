20dc2aa023cbf391d81ca8a1cdb7c02a112b1d2e
Benedict <benedict_zeng@live.com.au>
2020-09-11 16:10:18 +1000

    Beginning of number generation and setup. Have a list of test values for comparison

--- RadixSorting/Program.cs
+++ RadixSorting/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace RadixSorting
 {
@@ -6,7 +8,36 @@ namespace RadixSorting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  This program will generate a list of numbers, then use Radix Sort to sort them in ascending order.
+            //  At the end of the sorting, it will show how many steps were taken by the program, allowing it to be
+            //  compared to other sorting methods with a simple value.
+
+            int[] numArray;
+
+            if (!File.Exists("testvalues.txt"))
+            {
+                Console.WriteLine("testvalues.txt doesn't exist, creating new numbers");
+                Int32.TryParse(args[0], out int arrayLength);
+                numArray = RandomNumberGenerator.Generate(arrayLength);       //int[] numArray = new int[arrayLength];
+            } else
+            {
+                Console.WriteLine("testvalues.txt exists");
+                int arrayLength = File.ReadLines("testvalues.txt").Count();
+                numArray = new int[arrayLength];
+                numArray = Int32.TryParse(File.ReadLines("testvalues.txt").ToString, out int values);
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    //numArray[i] = File.
+                }
+            }
+
+            
+
+            /*foreach (int val in numArray)
+            {
+                Console.WriteLine(val);
+            }*/
         }
     }
 }
---
+++ RadixSorting/RandomNumberGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadixSorting
+{
+    class RandomNumberGenerator
+    {
+        static public int[] Generate(int arrayLength)
+        {
+            int[] numList = new int[arrayLength];
+
+            Random rnd = new Random();
+            for (int i = 0; i < numList.Length; i++)
+            {
+                numList[i] = rnd.Next(0, 1000);
+            }
+
+            return numList;
+        }
+    }
+}
