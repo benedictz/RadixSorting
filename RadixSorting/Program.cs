dfbb7f17d51ff33e94cc6510deca1f23cf1e29cc
Benedict <benedict_zeng@live.com.au>
2020-09-13 23:15:41 +1000

    Changed main to take string of text file rather than value

--- RadixSorting/Program.cs
+++ RadixSorting/Program.cs
@@ -11,20 +11,27 @@ namespace RadixSorting
             //  This program will generate a list of numbers, then use Radix Sort to sort them in ascending order.
             //  At the end of the sorting, it will show how many steps were taken by the program, allowing it to be
             //  compared to other sorting methods with a simple value.
+            //  Both Radix Bucket and Radix Counting will be implemented, and either or both can be run.
 
+            //  Create array for the initial values
             int[] numArray;
 
-            if (!File.Exists("testvalues.txt"))
+            //  If the file doesn't exist
+            //      Create a random set of values to populate the initial array
+            //  Else
+            //      Parse the file
+            //      Populate the array with the parsed values
+
+            if (!File.Exists(args[0]))
             {
-                Console.WriteLine("testvalues.txt doesn't exist, creating new numbers");
-                Int32.TryParse(args[0], out int arrayLength);
-                numArray = RandomNumberGenerator.Generate(arrayLength);       //int[] numArray = new int[arrayLength];
+                Console.WriteLine($"{args[0]} doesn't exist, creating new numbers");
+                numArray = RandomNumberGenerator.Generate(10);
             } else
             {
-                Console.WriteLine("testvalues.txt exists");
-                int arrayLength = File.ReadLines("testvalues.txt").Count();
+                Console.WriteLine($"{args[0]} exists");
+                int arrayLength = File.ReadLines(args[0]).Count();
                 numArray = new int[arrayLength];
-                numArray = Int32.TryParse(File.ReadLines("testvalues.txt").ToString, out int values);
+                //numArray = Int32.TryParse(File.ReadLines("testvalues.txt").ToString(), out int values);
 
                 for (int i = 0; i < arrayLength; i++)
                 {
